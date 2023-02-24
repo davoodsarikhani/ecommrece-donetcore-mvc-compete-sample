a57b2189cb1a5579d61f1f8801c6258a130e9710
davood sarikhani <davood.sarikhani@gmail.com>
2023-02-24 21:32:52 +0200

    step 3

--- eTicket/Data/Services/IActorsService.cs
+++ eTicket/Data/Services/IActorsService.cs
@@ -8,11 +8,11 @@ namespace eTicket.Data.Services
 {
     public interface IActorsService
     {
-        Task<IEnumerable<Actor>> GetAll();
-        Actor GetById(int id);
-        void AddActor(Actor actor);
-        Actor Update(int id, Actor actor);
-        void Delete(int id);
+        Task<IEnumerable<Actor>> GetAllAsync();
+        Task<Actor> GetByIdAsync(int id);
+        Task AddAsync(Actor actor);
+        Task<Actor> UpdateAsync(int id, Actor actor);
+        Task DeleteAsync(int id);
 
     }
 }
--- eTicket/Models/Actor.cs
+++ eTicket/Models/Actor.cs
@@ -11,9 +11,13 @@ namespace eTicket.Models
         [Key]
         public int Id { get; set; }
         [Display(Name = "profile picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         [Display(Name = "Full name")]
         public string FullName { get; set; }
+        [Required(ErrorMessage = "Biography is required")]
         [Display(Name = "Biography")]
         public string Bio { get; set; }
 
